bdd7b8714c3417bd90b8bf4d62911d52f8b1a607
Michael Sol <sol.mike.j@gmail.com>
2023-03-04 18:31:36 -0800

    fix up

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -5,12 +5,20 @@ namespace VenderOrderTracker.Models
 {
   public class Order
   {
-    
-    public Order() 
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+
+    public int Date { get; set; } //yyyymmdd -> 20230102 = January 1st, 2023
+    public List<Order> ListOfOrders { get; set;}
+    public Order(string name, string description, double price, int date) 
     {
-    
+      Name = name;
+      Description = description;
+      Price = price;
+      Date = date;
+      ListOfOrders.Add(this);
     }
-
-
+  
   }
 }
--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -5,12 +5,23 @@ namespace VenderOrderTracker.Models
 {
   public class Vendor
   {
-    
-    public Vendor() 
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Order> VendorsOrders { get; set;}
+
+    public Vendor(string name, string description) 
     {
-    
+      Name = name;
+      Description = description;
+      VendorsOrders = new List<Order>();
     }
 
+    public void AddOrder(Order newOrder){
+      VendorsOrders.Add(newOrder);
+    }
 
+    public void RemoveOrder(Order removeOrder){
+      VendorsOrders.Remove(removeOrder);
+    }
   }
 }
